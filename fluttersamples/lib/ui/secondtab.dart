ad051b2ba5ee8c335a0d739e16d0b41159271f86
tandong <852041173@qq.com>
2019-02-07 12:21:28 +0800

    更新网络请求
    更新网络请求

--- fluttersamples/lib/ui/secondtab.dart
+++ fluttersamples/lib/ui/secondtab.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'dart:io';
 import 'package:fluttersamples/widgets/ToolBar.dart';
+import 'dart:io';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class SecondTab extends StatefulWidget {
@@ -15,7 +15,7 @@ class SecondTabState extends State<SecondTab> {
   @override
   void initState() {
     super.initState();
-    get();
+    getHttpClient();
   }
 
   @override
@@ -31,21 +31,62 @@ class SecondTabState extends State<SecondTab> {
     );
   }
 
-  get() async {
-    // var responseBody;
-    // var url = 'http://192.168.1.130:8092/a/ab';
-    // var httpClient = new HttpClient();
-    // var request = await httpClient.getUrl(Uri.parse(url));
+  postHttpClient() async {
+    var url = "http://192.168.1.130:8092/a/r";
+    var httpClient = new HttpClient();
+    httpClient.postUrl(Uri.parse(url)).then((HttpClientRequest request) {
+      // Optionally set up headers...
+      // Optionally write to the request object...
+      // Then call close.
+      return request.close();
+    }).then((HttpClientResponse response) {
+      // Process the response.
+      if (response.statusCode == 200) {
+        response.transform(utf8.decoder).join().then((String string) {
+          print(string);
+        });
+      } else {
+        print("error");
+      }
+    });
+    ;
+  }
+
+  getHttpClient() async {
+    var url = "http://192.168.1.130:8092/a/ab";
+    var httpClient = new HttpClient();
+    httpClient.getUrl(Uri.parse(url)).then((HttpClientRequest request) {
+      // Optionally set up headers...
+      // Optionally write to the request object...
+      // Then call close.
+      return request.close();
+    }).then((HttpClientResponse response) {
+      // Process the response.
+      if (response.statusCode == 200) {
+        response.transform(utf8.decoder).join().then((String string) {
+          print(string);
+        });
+      } else {
+        print("error");
+      }
+    });
+
     // var response = await request.close();
+    // //判断是否请求成功
     // if (response.statusCode == 200) {
+    //   //拿到请求的数据
     //   responseBody = await response.transform(utf8.decoder).join();
-    //   responseBody = JSON.decode(responseBody);
+    //   print(responseBody);
+    //   //解析json，拿到对应的jsonArray数据
+    //   // var convertDataToJson = jsonDecode(responseBody)["data"]["datas"];
+    //   //更新数据
+    //   // setState(() {
+    //   //   data = convertDataToJson;
+    //   // });
+
     // } else {
     //   print("error");
     // }
-    // await responseBody['msg'].map((Map item) {
-    //   newTitle.add(item);
-    // }).toList();
   }
 
   void showToast() {
