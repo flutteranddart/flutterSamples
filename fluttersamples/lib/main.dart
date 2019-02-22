f5fc5c0f20bd1c3bae8e48a7326bacca32deff59
tandong <852041173@qq.com>
2019-02-22 22:44:12 +0800

    main
    main

--- fluttersamples/lib/main.dart
+++ fluttersamples/lib/main.dart
@@ -1,8 +1,8 @@
 /*
  * @Author: Tan Dong 
  * @Date: 2019-02-22 22:28:12 
- * @Last Modified by:   Tan Dong 
- * @Last Modified time: 2019-02-22 22:28:12 
+ * @Last Modified by: Tan Dong
+ * @Last Modified time: 2019-02-22 22:43:34
  */
 // Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
@@ -15,7 +15,6 @@ import 'package:flutter/services.dart';
 import 'package:fluttersamples/ui/home.dart';
 import 'package:fluttersamples/utils/GlobalPagesLocalizations.dart';
 import 'package:fluttersamples/utils/PageLocalizations.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
@@ -60,53 +59,36 @@ class WelcomePage extends StatefulWidget {
 
 class WelcomeState extends State<WelcomePage> {
   Timer timer;
-  SharedPreferences sharedPreferences;
 
   @override
   Widget build(BuildContext context) {
     return Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        color: Colors.white70,
-        // child:new Image.asset("assets/flutter-mark-square-64.png")
+        color: Color(0xFFf6f7f7),
         child: Stack(
           children: <Widget>[
             Image(
-              image: new AssetImage('assets/flutter-mark-square-64.png'),
+              image: new AssetImage('assets/flutter.png'),
               fit: BoxFit.none,
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height,
             ),
-            Center(
-              child: Text(
-                  '欢迎Flutter' + PageLocalizations.of(context).titleBarTitle,
+            Positioned(
+              left: 0,
+              right: 0,
+              top: 50,
+              child: Text(PageLocalizations.of(context).taskTitle,
                   textAlign: TextAlign.center,
                   softWrap: true,
                   style: TextStyle(
                       fontWeight: FontWeight.normal,
-                      color: Colors.orange,
+                      color: Colors.black,
+                      fontSize: 26,
                       decoration: TextDecoration.none)),
             ),
           ],
-        )
-        // Column(
-        //   children: <Widget>[
-        //     Text('欢迎Flutter',
-        //         textAlign: TextAlign.center,
-        //         softWrap: true,
-        //         style: TextStyle(
-        //             fontWeight: FontWeight.normal,
-        //             color: Colors.orange,
-        //             decoration: TextDecoration.none)),
-        //     Image(
-        //       image: new AssetImage('assets/flutter-mark-square-64.png'),
-        //       fit: BoxFit.none,
-        //       width: MediaQuery.of(context).size.width,
-        //       height: MediaQuery.of(context).size.height,
-        //     ),
-        //   ],
-        // )
-        );
+        ));
   }
 
   //FLUTTER_STORAGE_BASE_URL:https://storage.flutter-io.cn
